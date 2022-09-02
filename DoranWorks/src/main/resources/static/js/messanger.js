5b24d75c7345ddbb50e37712d251e0728bd1864a
Angela512 <57383916+Angela512@users.noreply.github.com>
2022-09-02 16:58:48 +0900

    멤버리스트 체크 해제 시 삭제 작업 ing

--- DoranWorks/src/main/resources/static/js/messanger.js
+++ DoranWorks/src/main/resources/static/js/messanger.js
@@ -13,12 +13,25 @@ $(function(){
 	$('.checkedMember').click(function(){
 		//체크된 회원번호 받음(이벤트가 발생한 태그에서 받아옴)
 		let mem_num = $(this).attr('data-num');
+		let isChecked = $(this).attr("checked", true);
+		let modifyUI = '';
+		if(isChecked){ //클릭되었으면
+			//댓글 수정폼 UI
+			modifyUI += '<input type="text" name="members" value="'+ mem_num +'">';
+			
+			//체크된 멤버 노출
+			$('.checked_div').append(modifyUI);
+		}else if(!isChecked){//클릭 안되어있거나 해제되었으면
+			//댓글 수정폼 UI
+			modifyUI += '<input type="text" name="members" value="False!!">';
+			
+			//체크된 멤버 노출
+			$('.checked_div').append(modifyUI);
+		}
 		
-		//댓글 수정폼 UI
-		let modifyUI = '<input type="text" name="members" value="'+ mem_num +'">';
 		
-		//체크된 멤버 노출
-		$('.checked_div').append(modifyUI);
+		
+		
 		
 		
 	});
